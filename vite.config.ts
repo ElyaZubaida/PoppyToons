7547dc096c21cf4f1632ccc118c7d1f8d0472d19
ElyaZubaida <164141641+ElyaZubaida@users.noreply.github.com>
2026-06-21 20:06:30 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
+  base: '/PoppyToons/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
